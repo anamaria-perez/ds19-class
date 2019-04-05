fe8c07077ad57e509422d4afdccbd21d4fade4a6
anamaria-perez <anamaria.perez118@gmail.com>
2019-04-05 14:56:44 -0700

    start making plots with ggplot

--- code/tidyr.R
+++ code/tidyr.R
@@ -35,6 +35,22 @@ fac_lengths <- bikenet_long %>%
   mutate(miles = metres / 1609)
 fac_lengths
 
+p <- ggplot(fac_lengths, aes(year, miles, 
+                             group = facility,
+                             color = facility))
 
+p + geom_line() # draw plot p as lines
+p + geom_point() # draw plot p as dots
+p + geom_line() + scale_y_log10() # draw plot p as lines with log10
+p + geom_line() + labs(title = "Change in Bike Facilities in Portland",
+                       subtitle = "2008 - 2013",
+                       caption = "source: Portland Metro") + # add a title and subtitle
+  xlab("Year") +
+  ylab("Total Miles")
+
+p2 <- ggplot(fac_lengths, aes(year, miles, 
+                             group = facility)) # create a new plot, p2
+p2 + geom_line(size = 1, color = "blue") + 
+  facet_wrap(~ facility) # plot each facility type in individual figures
 
 
