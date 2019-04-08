d000baa37ac00403c7b64d0853af1ef87abf6db2
anamaria-perez <anamaria.perez118@gmail.com>
2019-04-08 10:13:07 -0700

    starting to practice with functions

---
+++ code/intro-functions.R
@@ -0,0 +1,38 @@
+# convert farenheit to kelvin
+
+far_to_kel <- function(temp){
+  kelvin <- ((temp - 32)*(5/9)) + 273.15 #temp = argument, what you're inputting
+  return(kelvin)   #optional in R (automatically returns final output) but declares last value
+}
+
+far_to_kel(0)
+far_to_kel(32)
+
+# convert kelvin to celsius
+kel_to_cel <- function(temp){
+  cel <- (temp - 273.15)
+  return(cel)
+}
+
+kel_to_cel(0)
+
+# write a function converting Fahrenheight to Celsius 
+# by reusing the two functions you just wrote
+
+far_to_cel <- function(far){
+  cel <- kel_to_cel(far_to_kel(far))
+  return(cel)
+}
+
+far_to_cel(32)
+far_to_cel(212)
+
+# option 2
+far_to_cel_2 <- function(temp){
+  kel <- far_to_kel(temp)
+  cel <- kel_to_cel(kel)
+  return(cel)
+}
+
+far_to_cel_2(32)
+far_to_cel_2(212)
